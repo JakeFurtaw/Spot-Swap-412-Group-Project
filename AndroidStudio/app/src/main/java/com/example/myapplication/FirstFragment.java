a0f0799d40f8fe3adc0bd1220c269ff549bd79bd
Jake Furtaw <jake@nxtgencomputing.com>
2022-10-16 21:06:40 -0400

    added register page

--- AndroidStudio/app/src/main/java/com/example/myapplication/FirstFragment.java
+++ AndroidStudio/app/src/main/java/com/example/myapplication/FirstFragment.java
@@ -28,12 +28,19 @@ public class FirstFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
         binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                        .navigate(R.id.action_FirstFragment_to_fragment_login);
+            }
+        });
+
+        binding.buttonFirst2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_register);
             }
         });
     }
