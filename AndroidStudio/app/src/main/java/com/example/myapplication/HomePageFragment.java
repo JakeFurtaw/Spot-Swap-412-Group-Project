136f8509425a8444288bf66d2fd28068b23cdb40
EthanZiegler <ethan.ziegler1@gmail.com>
2022-10-31 16:31:20 -0400

    added SettingsFragment and AccountInfo and changed name of (SecondFragment.java --> homepage) and (glengaragemap --> spotfinder)

--- AndroidStudio/app/src/main/java/com/example/myapplication/HomePageFragment.java
+++ AndroidStudio/app/src/main/java/com/example/myapplication/HomePageFragment.java
@@ -9,11 +9,11 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-import com.example.myapplication.databinding.FragmentSecondBinding;
+import com.example.myapplication.databinding.FragmentHomepageBinding;
 
-public class SecondFragment extends Fragment {
+public class HomePageFragment extends Fragment {
 
-    private FragmentSecondBinding binding;
+    private FragmentHomepageBinding binding;
 
     @Override
     public View onCreateView(
@@ -21,7 +21,7 @@ public class SecondFragment extends Fragment {
             Bundle savedInstanceState
     ) {
 
-        binding = FragmentSecondBinding.inflate(inflater, container, false);
+        binding = FragmentHomepageBinding.inflate(inflater, container, false);
         return binding.getRoot();
 
     }
@@ -32,7 +32,7 @@ public class SecondFragment extends Fragment {
         binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
+                NavHostFragment.findNavController(HomePageFragment.this)
                         .navigate(R.id.action_SecondFragment_to_fragment_glenngaragemap2);
             }
         });
