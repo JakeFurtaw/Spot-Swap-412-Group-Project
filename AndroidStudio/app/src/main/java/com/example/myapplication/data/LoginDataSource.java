ecdcd607f41fbe12ff43ae50ac5b1dc9ba782c14
ethanziegler1 <ethan.ziegler1@gmail.com>
2022-12-06 14:33:23 -0500

    made it so it reads the name from the username edit text and then welcomes whatever was input there and then sends the user to the home page

--- AndroidStudio/app/src/main/java/com/example/myapplication/data/LoginDataSource.java
+++ AndroidStudio/app/src/main/java/com/example/myapplication/data/LoginDataSource.java
@@ -14,20 +14,8 @@ public class LoginDataSource {
     public Result<LoggedInUser> login(String username, String password) {
 
         try {
-            File file = new File("UserRepo.txt");
-            Scanner input = new Scanner(file);
-            //Read from file to see if username/password is stored.
-                while(input.hasNext()){
-                    if(input.nextLine().equals(username +"#"+ password)){
-                        LoggedInUser user = new LoggedInUser(username, username);
-                        return new Result.Success<>(user);
-                    }
-                }
             // TODO: handle loggedInUser authentication
-            LoggedInUser fakeUser =
-                    new LoggedInUser(
-                            java.util.UUID.randomUUID().toString(),
-                            "Jane Doe");
+            LoggedInUser fakeUser = new LoggedInUser(java.util.UUID.randomUUID().toString(), username);
             return new Result.Success<>(fakeUser);
         } catch (Exception e) {
             return new Result.Error(new IOException("Error logging in", e));
